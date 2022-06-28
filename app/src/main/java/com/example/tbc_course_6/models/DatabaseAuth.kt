6749ea024434efb1dc6d84455b27e90eca456f34
Luka Nikuradze <95241918+iTarley@users.noreply.github.com>
2022-06-28 23:20:31 +0400

    Update DatabaseAuth.kt

--- app/src/main/java/com/example/tbc_course_6/models/DatabaseAuth.kt
+++ app/src/main/java/com/example/tbc_course_6/models/DatabaseAuth.kt
@@ -4,7 +4,8 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 class DatabaseAuth {
-
+    //ეს მაქვს გატანილი ცალკე კლასში, რადგან წესით ეს უნდა იყოს .gitignore რომ არავის ჰქონდეს წვდომა ბაზასთან
+    //ეს არის საშინაო დავალება და რა თქმა უნდა მაგის გამო არ გავუწერ .gitignore
     val database = FirebaseDatabase.getInstance("https://tbc-course-6-1db9e-default-rtdb.europe-west1.firebasedatabase.app/").getReference("User")
     val auth = FirebaseAuth.getInstance()
 }
